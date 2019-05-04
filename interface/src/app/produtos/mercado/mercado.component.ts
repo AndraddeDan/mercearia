7a3a8d326b4e864ed42e6f7fd21424564a7b2970
Daniel Andrade <danielsilva97.andrade@gmail.com>
2019-05-04 00:07:52 -0300

    ultimas transições nos botões e cards

--- interface/src/app/produtos/mercado/mercado.component.ts
+++ interface/src/app/produtos/mercado/mercado.component.ts
@@ -4,12 +4,14 @@ import { text } from '@angular/core/src/render3';
 import { ProdutosService } from '../produtos.service';
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'app-mercadoLine',
     templateUrl: 'mercado.component.html',
     styleUrls: ['../../app.component.scss']
 })
 export class MercadoComponent implements OnInit {
 
+    // tslint:disable-next-line:ban-types
     stores: Object[] = [];
 
     produtosService: ProdutosService;
@@ -26,12 +28,12 @@ export class MercadoComponent implements OnInit {
         this.listStores(1);
     }
 
-    listStores(id){
+    listStores(id) {
         this.produtosService.getInfoProduto(id).subscribe(info => {
             this.stores = info,
             this.stores.forEach(store => {
                 store.largura = store.quantity_in_store * 100;
-            })
+            });
         });
     }
 }
